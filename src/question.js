cc47b726dc72f7efe97aafd3cffb2bc13d18af08
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-04-16 23:13:33 +0200

    setup the Project structure , and implement the filter and average  difficulty methodes using Array special methodes

--- src/question.js
+++ src/question.js
@@ -1,7 +1,24 @@
 class Question {
     // YOUR CODE HERE:
     //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+    // Fisher-Yates shuffle algorithm
+    shuffleChoices(){
+        for(let i = this.choices.length - 1 ; i > 0 ; i--){
+            // generating a randoum index between 0 and i
+            const j = Math.floor(Math.random() * (i + 1));
 
-    // 2. shuffleChoices()
+            // swaping the elments
+            let temp = this.choices[i];
+            this.choices[i] = this.choices[j];
+            this.choices[j] = temp;
+        }
+    }
 }
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,57 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
 
-    // 2. getQuestion()
+    }
+
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        for(let i = this.questions.length - 1 ; i > 0 ; i--){
+            // generating a randoum index between 0 and i
+            const j = Math.floor(Math.random() * (i + 1));
 
-    // 5. checkAnswer(answer)
+            // swaping the elments
+            let temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+        }
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer){
+        if(answer === this.getQuestion().answer)
+            this.correctAnswers++;
+    }
+
+    hasEnded(){
+        if(this.currentQuestionIndex < this.questions.length)
+            return false;
+        if(this.currentQuestionIndex === this.questions.length)
+            return true;
+    }
+    filterQuestionsByDifficulty(difficulty){
+        if( difficulty >= 1  && difficulty <= 3 )
+            return this.questions.filter(question => question.difficulty === difficulty);
+        return this.questions;
+    }
+    averageDifficulty(){
+        const sumDifficulty = this.questions.reduce((acc,question) => {
+            return acc + question.difficulty;
+        } , 0);
+        return sumDifficulty / this.questions.length;
+    }
 }
+
+
+
